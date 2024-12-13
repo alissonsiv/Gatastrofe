bd2888e490ea230891787421635dc4af7f9d0915
Alisson <alissonmoura890@gmail.com>
2024-12-12 23:52:42 -0300

    Ajustes de precisão na ativação dos alvos

---
+++ Scripts/Gatos.gd
@@ -0,0 +1,36 @@
+class_name Target
+
+extends Area2D
+
+var speed = 10
+
+class TargetType:
+	var color: Color
+	var speed: int
+
+	func _init(_color: Color, _speed: int) -> void:
+		self.color = _color
+		self.speed = _speed
+
+	func to_dict() -> Dictionary:
+		return {color = color.to_html(), speed = speed}
+
+	static func from_dict(dict: Dictionary) -> TargetType:
+		return TargetType.new(Color(dict["color"]), dict["speed"])
+
+func _ready() -> void:
+	$Sprite2D.texture = TargetData.GATOS.pick_random()
+
+func definir_tipo(tipo_balao: TargetType) -> void:
+	$AnimatedSprite2D.modulate = tipo_balao.color
+	speed = tipo_balao.speed
+
+func _physics_process(delta: float) -> void:
+	position += gravity_direction.normalized() * speed * delta
+
+func destroy():
+	$AnimatedSprite2D.play("Burst")
+	gravity_direction = -gravity_direction
+	$CollisionShape2D.queue_free()
+	$Timer.connect("timeout", queue_free)
+	$Timer.start()
